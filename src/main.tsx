393bd41faa97199a42fc9c69fe8251cfe3161adb
franca visokolskis <frany.ps@gmail.com>
2025-10-10 13:55:59 -0300

    add: router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/destination/:id" element={<App />} />
+        <Route path="*" element={<p>Not found 404</p>} /> 
+        {/* TODO: add a 404 page */}
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
