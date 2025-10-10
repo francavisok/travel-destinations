b0f6093faf67098ab37a8f9627a6dfd12bf242fa
franca visokolskis <frany.ps@gmail.com>
2025-10-10 15:48:02 -0300

    add: shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { useParams } from "react-router";
+import "./App.css";
+import { Badge } from "./components/ui/badge";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,28 +9,7 @@ function App() {
   const { id } = useParams<{ id: string }>();
   console.log("id", id);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Badge variant="default">Testing shadcn</Badge>
   );
 }
 
